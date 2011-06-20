c38806367437c682f1606c93631fab91ae583762
g.hillairet <g.hillairet@gmail.com>
2011-06-20 23:44:37 +0100

    first commit for emfjson - emf binding for json apis

---
+++ tests/com.emfjson.junit/src/com/emfjson/junit/rotten/Release.java
@@ -0,0 +1,81 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package com.emfjson.junit.rotten;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Release</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link com.emfjson.junit.rotten.Release#getTheater <em>Theater</em>}</li>
+ *   <li>{@link com.emfjson.junit.rotten.Release#getDvd <em>Dvd</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see com.emfjson.junit.rotten.RottenPackage#getRelease()
+ * @model
+ * @generated
+ */
+public interface Release extends EObject {
+	/**
+	 * Returns the value of the '<em><b>Theater</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Theater</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Theater</em>' attribute.
+	 * @see #setTheater(String)
+	 * @see com.emfjson.junit.rotten.RottenPackage#getRelease_Theater()
+	 * @model
+	 * @generated
+	 */
+	String getTheater();
+
+	/**
+	 * Sets the value of the '{@link com.emfjson.junit.rotten.Release#getTheater <em>Theater</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Theater</em>' attribute.
+	 * @see #getTheater()
+	 * @generated
+	 */
+	void setTheater(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Dvd</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Dvd</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Dvd</em>' attribute.
+	 * @see #setDvd(String)
+	 * @see com.emfjson.junit.rotten.RottenPackage#getRelease_Dvd()
+	 * @model
+	 * @generated
+	 */
+	String getDvd();
+
+	/**
+	 * Sets the value of the '{@link com.emfjson.junit.rotten.Release#getDvd <em>Dvd</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Dvd</em>' attribute.
+	 * @see #getDvd()
+	 * @generated
+	 */
+	void setDvd(String value);
+
+} // Release
