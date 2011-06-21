b99f8d194a77e5030bac129fe1a51929852e3ef7
ghillairet <g.hillairet@gmail.com>
2011-06-21 18:46:09 +0100

    added sample

---
+++ tests/com.emfjson.junit/src/com/emfjson/model/rotten/Poster.java
@@ -0,0 +1,135 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package com.emfjson.model.rotten;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Poster</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link com.emfjson.model.rotten.Poster#getThumbnail <em>Thumbnail</em>}</li>
+ *   <li>{@link com.emfjson.model.rotten.Poster#getProfile <em>Profile</em>}</li>
+ *   <li>{@link com.emfjson.model.rotten.Poster#getDetailed <em>Detailed</em>}</li>
+ *   <li>{@link com.emfjson.model.rotten.Poster#getOriginal <em>Original</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see com.emfjson.model.rotten.RottenPackage#getPoster()
+ * @model
+ * @generated
+ */
+public interface Poster extends EObject {
+	/**
+	 * Returns the value of the '<em><b>Thumbnail</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Thumbnail</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Thumbnail</em>' attribute.
+	 * @see #setThumbnail(String)
+	 * @see com.emfjson.model.rotten.RottenPackage#getPoster_Thumbnail()
+	 * @model
+	 * @generated
+	 */
+	String getThumbnail();
+
+	/**
+	 * Sets the value of the '{@link com.emfjson.model.rotten.Poster#getThumbnail <em>Thumbnail</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Thumbnail</em>' attribute.
+	 * @see #getThumbnail()
+	 * @generated
+	 */
+	void setThumbnail(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Profile</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Profile</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Profile</em>' attribute.
+	 * @see #setProfile(String)
+	 * @see com.emfjson.model.rotten.RottenPackage#getPoster_Profile()
+	 * @model
+	 * @generated
+	 */
+	String getProfile();
+
+	/**
+	 * Sets the value of the '{@link com.emfjson.model.rotten.Poster#getProfile <em>Profile</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Profile</em>' attribute.
+	 * @see #getProfile()
+	 * @generated
+	 */
+	void setProfile(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Detailed</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Detailed</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Detailed</em>' attribute.
+	 * @see #setDetailed(String)
+	 * @see com.emfjson.model.rotten.RottenPackage#getPoster_Detailed()
+	 * @model
+	 * @generated
+	 */
+	String getDetailed();
+
+	/**
+	 * Sets the value of the '{@link com.emfjson.model.rotten.Poster#getDetailed <em>Detailed</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Detailed</em>' attribute.
+	 * @see #getDetailed()
+	 * @generated
+	 */
+	void setDetailed(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Original</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Original</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Original</em>' attribute.
+	 * @see #setOriginal(String)
+	 * @see com.emfjson.model.rotten.RottenPackage#getPoster_Original()
+	 * @model
+	 * @generated
+	 */
+	String getOriginal();
+
+	/**
+	 * Sets the value of the '{@link com.emfjson.model.rotten.Poster#getOriginal <em>Original</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Original</em>' attribute.
+	 * @see #getOriginal()
+	 * @generated
+	 */
+	void setOriginal(String value);
+
+} // Poster
