6bc053144f6a54b3b8eb0a0086b65c5066a5bf9f
ghillairet <g.hillairet@gmail.com>
2011-11-13 17:33:09 +0100

    add static class holding options to be use with JsResource

---
+++ bundles/org.eclipselabs.emfjson/src/org/eclipselabs/emfjson/EJs.java
@@ -0,0 +1,9 @@
+package org.eclipselabs.emfjson;
+
+public final class EJs {
+
+	public static final String OPTION_STREAMING_JSON = "OPTION_STREAMING_JSON";
+	public static final String OPTION_ROOT_ELEMENT = "OPTION_ROOT_ELEMENT";
+	public static final String OPTION_URL_PARAMETERS = "OPTION_URL_PARAMETERS";
+	public static final String OPTION_SERIALIZE_TYPE = "OPTION_SERIALIZE_TYPE";
+}
