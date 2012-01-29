1beca6f94edb12af5a353610483146c7360e77f7
ghillairet <g.hillairet@gmail.com>
2012-01-29 21:35:47 +0100

    Updated test for couchdb

--- tests/org.eclipselabs.emfjson.couchdb.junit/src/org/eclipselabs/emfjson/couchdb/junit/tests/TestStoreDocument.java
+++ tests/org.eclipselabs.emfjson.couchdb.junit/src/org/eclipselabs/emfjson/couchdb/junit/tests/TestStoreDocument.java
@@ -4,23 +4,17 @@ import java.io.IOException;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipselabs.emfjson.couchdb.CouchDBHandler;
-import org.eclipselabs.emfjson.couchdb.junit.model.ModelFactory;
-import org.eclipselabs.emfjson.couchdb.junit.model.User;
-import org.junit.Before;
+import org.eclipselabs.emfjson.couchdb.junit.support.TestSupport;
+import org.eclipselabs.emfjson.junit.model.ModelFactory;
+import org.eclipselabs.emfjson.junit.model.User;
 import org.junit.Test;
 
-public class TestStoreDocument {
-
-	protected ResourceSet resourceSet;
-	private final String url = "http://127.0.0.1:5984/emfjson_model/users/";
-	
-	@Before
-	public void tearUp() {
-		resourceSet = new ResourceSetImpl();
-		resourceSet.getURIConverter().getURIHandlers().add(0, new CouchDBHandler());
+public class TestStoreDocument extends TestSupport {
+		
+	@Test
+	public void testCreateDatabase() throws IOException {
+		Resource resource = resourceSet.createResource(URI.createURI(url));
+		resource.save(null);
 	}
 	
 	@Test
@@ -28,16 +22,44 @@ public class TestStoreDocument {
 		Resource resource = resourceSet.createResource(URI.createURI(url));
 		
 		User u1 = ModelFactory.eINSTANCE.createUser();
-		u1.set_id("1");
+		u1.setUserId("1");
+		u1.setName("John");
+		
+		resource.getContents().add(u1);
+		
+		resource.save(null);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testStoreDocumentWithTwoRootObjects() throws IOException {
+		Resource resource = resourceSet.createResource(URI.createURI(url));
+		
+		User u1 = ModelFactory.eINSTANCE.createUser();
+		u1.setUserId("1");
 		u1.setName("John");
 		User u2 = ModelFactory.eINSTANCE.createUser();
-		u2.set_id("2");
+		u2.setUserId("2");
 		u2.setName("Paul");
-		u1.getKnows().add(u2);
+		u1.getFriends().add(u2);
 		
 		resource.getContents().add(u1);
 		resource.getContents().add(u2);
 		
+		resource.save(null);
+	}
+	
+	@Test
+	public void testStoreDocumentWithOneObjectAndUpdate() throws IOException {
+		Resource resource = resourceSet.createResource(URI.createURI(url));
+		
+		User u1 = ModelFactory.eINSTANCE.createUser();
+		u1.setUserId("1");
+		u1.setName("John");
+		
+		resource.getContents().add(u1);
+		resource.save(null);
+		
+		u1.setName("John Smith");
 		resource.save(null);
 	}
 }
