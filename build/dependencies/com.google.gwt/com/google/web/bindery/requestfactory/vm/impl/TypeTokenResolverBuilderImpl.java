e741674915abd271b5c0d4c646781ae2e32f7933
ghillairet <g.hillairet@gmail.com>
2012-02-25 16:25:21 +0100

    first commit for gwt port of emfjs and gwt p2 repository for tycho build

---
+++ build/dependencies/com.google.gwt/com/google/web/bindery/requestfactory/vm/impl/TypeTokenResolverBuilderImpl.java
@@ -0,0 +1,8 @@
+package com.google.web.bindery.requestfactory.vm.impl;
+public class TypeTokenResolverBuilderImpl extends com.google.web.bindery.requestfactory.vm.impl.TypeTokenResolver.Builder {
+public TypeTokenResolverBuilderImpl() {
+addTypeToken("8KVVbwaaAtl6KgQNlOTsLCp9TIU=", "com.google.web.bindery.requestfactory.shared.ValueProxy");
+addTypeToken("FXHD5YU0TiUl3uBaepdkYaowx9k=", "com.google.web.bindery.requestfactory.shared.BaseProxy");
+addTypeToken("w1Qg$YHpDaNcHrR5HZ$23y518nA=", "com.google.web.bindery.requestfactory.shared.EntityProxy");
+}
+}
