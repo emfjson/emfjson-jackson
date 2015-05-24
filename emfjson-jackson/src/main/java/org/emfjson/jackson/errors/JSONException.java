2dade22f4186c1b54bf627284d3a121e9e7aa5e6
ghillairet <g.hillairet@gmail.com>
2015-05-24 17:54:31 +0200

    [#50] Enable strict mode during parsing of json files
    Add errors in resource during deserialisation if exception occurs.

---
+++ emfjson-jackson/src/main/java/org/emfjson/jackson/errors/JSONException.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2015 Guillaume Hillairet.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Guillaume Hillairet - initial API and implementation
+ */
+package org.emfjson.jackson.errors;
+
+import org.eclipse.emf.ecore.resource.Resource;
+
+import com.fasterxml.jackson.core.JsonLocation;
+
+public class JSONException extends Exception implements Resource.Diagnostic {
+
+	private final String location;
+	private final int line;
+	private final int column;
+
+	public JSONException(Exception e, JsonLocation location) {
+		super(e);
+		this.location = location.toString();
+		this.line = location.getLineNr();
+		this.column = location.getColumnNr();
+	}
+
+	@Override
+	public String getLocation() {
+		return location;
+	}
+
+	@Override
+	public int getLine() {
+		return line;
+	}
+
+	@Override
+	public int getColumn() {
+		return column;
+	}
+
+}
