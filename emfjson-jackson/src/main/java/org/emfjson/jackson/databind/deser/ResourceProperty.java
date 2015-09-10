e7d28b22ce7e16623e9950d7677c18282e71212b
ghillairet <g.hillairet@gmail.com>
2015-09-10 13:28:21 +0200

    Make ResourceProperty inherit the abstract BeanProperty.Std instead of implementing the interface, to avoid incompatibility with jackson 2.6.0

--- emfjson-jackson/src/main/java/org/emfjson/jackson/databind/deser/ResourceProperty.java
+++ emfjson-jackson/src/main/java/org/emfjson/jackson/databind/deser/ResourceProperty.java
@@ -11,24 +11,28 @@
  */
 package org.emfjson.jackson.databind.deser;
 
-import com.fasterxml.jackson.databind.*;
-import com.fasterxml.jackson.databind.introspect.AnnotatedMember;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonObjectFormatVisitor;
+import com.fasterxml.jackson.databind.BeanProperty;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.PropertyMetadata;
+import com.fasterxml.jackson.databind.PropertyName;
+import com.fasterxml.jackson.databind.introspect.AnnotationMap;
 import com.fasterxml.jackson.databind.type.SimpleType;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.emfjson.common.ReferenceEntries;
 
-import java.lang.annotation.Annotation;
-
-public class ResourceProperty implements BeanProperty {
+public class ResourceProperty extends BeanProperty.Std {
 
 	private final ResourceSet resourceSet;
 	private final Resource resource;
 	private final ReferenceEntries entries;
 
+	private static final JavaType type = SimpleType.construct(EObject.class);
+	private static final PropertyName propertyName = PropertyName.construct("contents", "org.emfjson.resource");
+
 	public ResourceProperty(ResourceSet resourceSet, Resource resource, ReferenceEntries entries) {
+		super(propertyName, type, propertyName, new AnnotationMap(), null, PropertyMetadata.STD_REQUIRED);
 		this.resourceSet = resourceSet;
 		this.resource = resource;
 		this.entries = entries;
@@ -45,53 +49,5 @@ public class ResourceProperty implements BeanProperty {
 	public ReferenceEntries getEntries() {
 		return entries;
 	}
-
-	@Override
-	public String getName() {
-		return "contents";
-	}
-
-	@Override
-	public PropertyName getFullName() {
-		return PropertyName.construct("contents", "org.emfjson.resource");
-	}
-
-	@Override
-	public JavaType getType() {
-		return SimpleType.construct(EObject.class);
-	}
-
-	@Override
-	public PropertyName getWrapperName() {
-		return null;
-	}
-
-	@Override
-	public PropertyMetadata getMetadata() {
-		return null;
-	}
-
-	@Override
-	public boolean isRequired() {
-		return false;
-	}
-
-	@Override
-	public <A extends Annotation> A getAnnotation(Class<A> acls) {
-		return null;
-	}
-
-	@Override
-	public <A extends Annotation> A getContextAnnotation(Class<A> acls) {
-		return null;
-	}
-
-	@Override
-	public AnnotatedMember getMember() {
-		return null;
-	}
-
-	@Override
-	public void depositSchemaProperty(JsonObjectFormatVisitor objectVisitor) throws JsonMappingException {}
 
 }
